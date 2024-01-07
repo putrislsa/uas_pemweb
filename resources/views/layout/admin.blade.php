3bdb9ae7603b504c9f492bf80099d6f77e302277
putrislsa <putrislsaaa@gmaill.com>
2024-01-07 18:19:06 +0700

    baru lg

--- resources/views/layout/admin.blade.php
+++ resources/views/layout/admin.blade.php
@@ -166,7 +166,7 @@
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
             <a href="index3.html" class="brand-link">
-                <img src="{{asset('template/dist/img/hotellogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+                <img src="{{asset('template/dist/img/hotel_baru.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-light">Hotel 3sKs</span>
             </a>
 
@@ -175,7 +175,7 @@
                 <!-- Sidebar user panel (optional) -->
                 <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                     <div class="image">
-                        <img src="{{asset('template/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
+                        <img src="{{asset('template/dist/img/ps.jpg') }}" class="img-circle elevation-2" alt="User Image">
                     </div>
                     <div class="info">
                         <a href="#" class="d-block">{{ Auth::user()->name }}</a>
